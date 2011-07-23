248a492939734d3c5c0017434dee912d77877c8d
Thomas Baum <thomas.baum@atns.de>
2011-07-23 20:16:39 +0200

    initial version

---
+++ backend/backend.rb
@@ -0,0 +1,33 @@
+require "sinatra"
+require "neo4j"
+
+unless $NEO4J_SERVER
+# please ensure a clean database for this example!
+  Neo4j::Config[:storage_path] = '~/Downloads/neo4j-community-1.4/data/graph.db'
+end
+
+class Person
+   include Neo4j::NodeMixin
+   property :name
+   has_n :in_love
+   index :name
+end
+
+def create_graph()
+  Neo4j::Transaction.run do
+    return if MyNode.find("name: I").first
+
+    me = MyNode.new :name => "I" 
+    you = MyNode.new :name => "You"
+    me.in_love << you
+  end
+end
+
+create_graph
+
+get "/connections/:name" do |name|
+  me = Person.find("name: #{name}").first
+  return unless me
+
+  Hash[ me.in_love.collect{ |r| [:love,r.name ] } ].to_json
+end
---
+++ backend/config.ru
@@ -0,0 +1,3 @@
+require './backend.rb'
+
+run Sinatra::Application
