69d7e71b313b4700f233b225104b30faa5f91693
Thomas Baum <thomas.baum@atns.de>
2011-07-24 13:15:21 +0200

    x

--- backend/config.ru
+++ backend/config.ru
@@ -2,4 +2,4 @@
 
 require 'backend.rb'
 
-run Sinatra::Application
+run Backend
--- backend/lib/backend.rb
+++ backend/lib/backend.rb
@@ -1,40 +1,43 @@
 require "sinatra"
 require "neo4j"
 
-puts "TEST:"
-p $NEO4J_SERVER
-p NEO4J_SERVER
-puts "xxxxxx"
+class Backend < Sinatra::Base
 
-unless $NEO4J_SERVER
-# please ensure a clean database for this example!
-  Neo4j::Config[:storage_path] = '~/Downloads/neo4j-community-1.4/data/graph.db'
-end
-
-class Person
-   include Neo4j::NodeMixin
-   property :name
-   has_n :in_love
-   index :name
-end
-
-def create_graph()
-  Neo4j::Transaction.run do
-    return if Person.find("name: I").first
-
-    me = Person.new :name => "I" 
-    you = Person.new :name => "You"
-    me.in_love << you
+  def initialize()
+    puts "init sini"
+    p $NEO4J_SERVER
+    unless $NEO4J_SERVER
+    # please ensure a clean database for this example!
+      Neo4j::Config[:storage_path] = '~/Downloads/neo4j-community-1.4/data/graph.db'
+    end
+    create_graph
+  end
+  
+  class Person
+    include Neo4j::NodeMixin
+    property :name
+    has_n :in_love
+    index :name
+  end
+  
+  def create_graph()
+    Neo4j::Transaction.run do
+      return if Person.find("name: I").first
+  
+      me = Person.new :name => "I" 
+      you = Person.new :name => "You"
+      me.in_love << you
+    end
+  end
+  
+  
+  get "/connections/:name" do |name|
+   p $NEO4J_SERVER
+  
+   me = Person.find("name: #{name}").first
+    return unless me
+  
+    Hash[ me.in_love.collect{ |r| [:love,r.name + "x" ] } ].to_json
   end
 end
-
-create_graph
-
-get "/connections/:name" do |name|
- p $NEO4J_SERVER
-
- me = Person.find("name: #{name}").first
-  return unless me
-
-  Hash[ me.in_love.collect{ |r| [:love,r.name + "x" ] } ].to_json
-end
+  
