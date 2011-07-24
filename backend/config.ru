97a6827624903822b3259f769a135f6038ed04d6
Thomas Baum <thomas.baum@atns.de>
2011-07-24 14:01:33 +0200

    x

--- backend/config.ru
+++ backend/config.ru
@@ -2,4 +2,4 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "lib"))
 
 require 'backend'
 
-run Sinatra::Application
+run Backend
--- backend/lib/backend.rb
+++ backend/lib/backend.rb
@@ -8,35 +8,36 @@ class Person
   index :name
 end
 
-def create_graph()
-  Neo4j::Transaction.run do
-    return if Person.find("name: I").first
-
-    me = Person.new :name => "I" 
-    you = Person.new :name => "You"
-    me.in_love << you
+class Backend < Sinatra::Base
+  def create_graph()
+    Neo4j::Transaction.run do
+      return if Person.find("name: I").first
+  
+      me = Person.new :name => "I" 
+      you = Person.new :name => "You"
+      me.in_love << you
+    end
   end
-end
-
-
-configure do
-  puts "init sini"
-  puts "x#{ $NEO4J_SERVER }x"
-    unless $NEO4J_SERVER
-  # please ensure a clean database for this example!
-#    Neo4j::Config[:storage_path] = '~/Downloads/neo4j-community-1.4/data/graph.db'
+  
+  
+  configure do
+    puts "init sini"
+#    puts $NEO4J_SERVER
+      unless $NEO4J_SERVER
+    # please ensure a clean database for this example!
+      Neo4j::Config[:storage_path] = '~/Downloads/neo4j-community-1.4/data/graph.db'
+    end
+    create_graph
   end
-  create_graph
-end
-
-get "/connections/:name" do |name|
- puts "doget"
- puts "x#{ $NEO4J_SERVER }x"
-
- me = Person.find("name: #{name}").first
-  return unless me
-
-  Hash[ me.in_love.collect{ |r| [:love,r.name + " yy" ] } ].to_json
-end
-
+  
+  get "/connections/:name" do |name|
+   puts "doget"
+#   puts $NEO4J_SERVER }x"
+  
+   me = Person.find("name: #{name}").first
+    return unless me
+  
+    Hash[ me.in_love.collect{ |r| [:love,r.name + " yy" ] } ].to_json
+  end
+end  
   
